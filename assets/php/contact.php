6cce2390be76641c9ac45bd3cc4ad9ca60b0cdd6
Milan <milanso@gmail.com>
2017-09-07 20:16:14 +0200

    v1.12

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -43,8 +43,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'info@example.com';  // please change this email id
-	$subject = 'Contact Form : Titan - The best downloaded template ever';
+	$to = 'milan.celap@gmail.com';  // please change this email id
+	$subject = 'Contact Form : Get Sheet Done';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
